74c27f1f738977de473240f2d98c35f8849279bb
황아영 <ayoganatanatda@naver.com>
2022-09-21 21:45:47 +0900

    Feat: Complaint Service 파일 저장 로직 추가

--- src/main/java/server/knucd/complaint/service/ComplaintService.java
+++ src/main/java/server/knucd/complaint/service/ComplaintService.java
@@ -2,22 +2,31 @@ package server.knucd.complaint.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import server.knucd.complaint.dto.CreateComplaintForm;
 import server.knucd.complaint.entity.Category;
 import server.knucd.complaint.entity.Complaint;
-import server.knucd.complaint.entity.Location;
 import server.knucd.complaint.repository.ComplaintRepository;
+import server.knucd.exception.NotFoundException;
+import server.knucd.file.service.FileService;
 import server.knucd.member.entity.Member;
+import server.knucd.member.repository.MemberRepository;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ComplaintService {
 
     private final ComplaintRepository complaintRepository;
 
-    public void save(CreateComplaintForm form) {
+    private final MemberRepository memberRepository;
+    private final FileService fileService;
+
+    @Transactional
+    public Long save(CreateComplaintForm form) throws IOException {
         // sample member
         Member member = Member.builder()
                 .name("테스트")
@@ -25,19 +34,24 @@ public class ComplaintService {
                 .email("123@abc.com")
                 .address("대구 북구 대학로 80")
                 .build();
-
-        // 요기에서 파일 스토리지에 저장하고 path 추출
-        String path = "/image.png";
+        memberRepository.save(member);
 
         Complaint complaint = Complaint.builder()
                 .writer(member)
                 .title(form.getTitle())
                 .content(form.getContent())
-                .file(path)
                 .category(form.getCategory())
+                .latitude(form.getLatitude())
+                .longitude(form.getLongitude())
                 .build();
 
         complaintRepository.save(complaint);
+
+        if(form.getFile() != null) {
+            String path = fileService.uploadPNG(complaint.getId() + "", form.getFile().getBytes());
+            complaint.setFile(path);
+        }
+        return complaint.getId();
     }
 
     public List<Complaint> findAllByCategory(Category category) {
@@ -50,10 +64,16 @@ public class ComplaintService {
         return complaints;
     }
 
+    public Complaint findById(Long id) {
+        Complaint complaint = complaintRepository.findById(id).orElseThrow(() -> new NotFoundException("민원이 존재하지 않습니다."));
+        return complaint;
+    }
+
     // 감정 표현 추가 기능 구현 예정
 
+    @Transactional
     public void deleteById(Long id) {
-        Complaint complaint = complaintRepository.findById(id).orElseThrow();
+        Complaint complaint = complaintRepository.findById(id).orElseThrow(() -> new NotFoundException("민원이 존재하지 않습니다."));
         complaintRepository.delete(complaint);
     }
 
