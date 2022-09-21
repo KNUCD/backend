bde88f3777b20d42d9d480cef922611f035c667d
황아영 <ayoganatanatda@naver.com>
2022-09-21 16:52:54 +0900

    Feat: Complaint Repository 추가

---
+++ src/main/java/server/knucd/complaint/repository/ComplaintRepository.java
@@ -0,0 +1,49 @@
+package server.knucd.complaint.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import server.knucd.complaint.entity.Category;
+import server.knucd.complaint.entity.Complaint;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class ComplaintRepository {
+
+    private final EntityManager em;
+
+    public void save(Complaint complaint) {
+        em.persist(complaint);
+    }
+
+    public Optional<Complaint> findById(Long id) {
+        return Optional.ofNullable(em.find(Complaint.class, id));
+    }
+
+    public List<Complaint> findAllByCategory(Category category) {
+        return em.createQuery("select c from Complaint c " +
+                "where c.category = :category " +
+                "order by c.id desc",
+                Complaint.class)
+                .setParameter("category", category)
+                .getResultList();
+    }
+
+    public List<Complaint> findAllByCategory(Category category, int page, int size) {
+        return em.createQuery("select c from Complaint c " +
+                "where c.category = :category " +
+                "order by c.id desc",
+                Complaint.class)
+                .setParameter("category", category)
+                .setMaxResults(size)
+                .setFirstResult((page-1)*size)
+                .getResultList();
+    }
+
+    public void delete(Complaint complaint) {
+        em.remove(complaint);
+    }
+}
