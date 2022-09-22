3f2dc4205359100111dc6df1b75d8c414f0b0842
PPakSang <ppm5377@naver.com>
2022-09-22 09:34:32 +0900

    Feat: Redis 추가

--- src/main/java/server/knucd/HackathonApplication.java
+++ src/main/java/server/knucd/HackathonApplication.java
@@ -2,10 +2,12 @@ package server.knucd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@EnableCaching
 @EnableJpaAuditing
+@SpringBootApplication
 public class HackathonApplication {
 
     public static void main(String[] args) {
